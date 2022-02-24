1a6dbf921601732f9d08b9e7399b7a24a7f4dd30
RooSoft <marc@roosoft.com>
2022-02-24 09:38:17 -0500

    document Bolt.Sips.start_link

--- lib/neo4j.ex
+++ lib/neo4j.ex
@@ -2,6 +2,7 @@ defmodule LightningGraph.Neo4j do
   def get_connection do
     config = Application.get_env(:bolt_sips, Bolt)
 
+    # Will create a GenServer if none exist, otherwise will return the existing one
     {:ok, _neo} = Bolt.Sips.start_link(config)
 
     Bolt.Sips.conn()
