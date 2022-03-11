1381758531fd4307331853f3c10f657fba95d538
RooSoft <marc@roosoft.com>
2022-03-11 13:50:58 -0500

    added a parameter to GraphUpdater.start_link

--- lib/neo4j/lnd/graph_updater.ex
+++ lib/neo4j/lnd/graph_updater.ex
@@ -3,7 +3,7 @@ defmodule LightningGraph.Neo4j.Lnd.GraphUpdater do
 
   require Logger
 
-  def start_link() do
+  def start_link(_) do
     GenServer.start_link(__MODULE__, nil, name: __MODULE__)
   end
 
