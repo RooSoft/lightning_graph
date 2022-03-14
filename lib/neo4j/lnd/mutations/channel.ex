4d1a2369ed859b87607afa90f627739df8089015
RooSoft <marc@roosoft.com>
2022-03-14 11:08:00 -0400

    return more channel update info

--- lib/neo4j/lnd/mutations/channel.ex
+++ lib/neo4j/lnd/mutations/channel.ex
@@ -2,25 +2,24 @@ defmodule LightningGraph.Neo4j.Lnd.Mutations.Channel do
   require Logger
 
   def update(conn, channel_edge_update) do
-    routing_policy = channel_edge_update.routing_policy
+    channel_fields = convert(channel_edge_update)
 
     set_statement =
       []
-      |> maybe_add(channel_edge_update.capacity, "capacity")
-      |> maybe_add(routing_policy.fee_base_msat, "base_fee")
-      |> maybe_add(routing_policy.fee_rate_milli_msat, "fee_rate")
-      |> maybe_add(routing_policy.disabled, "disabled")
+      |> maybe_add(channel_fields.capacity, "capacity")
+      |> maybe_add(channel_fields.base_fee, "base_fee")
+      |> maybe_add(channel_fields.fee_rate, "fee_rate")
+      |> maybe_add(channel_fields.disabled, "disabled")
       |> Enum.join(", ")
 
     query = """
     MATCH (node)-[c:CHANNEL {lnd_id: "#{channel_edge_update.chan_id}"}]-(node)
-    SET c += { #{set_statement} }
-    RETURN c;
+    SET c += { #{set_statement} };
     """
 
     Bolt.Sips.query!(conn, query)
 
-    %{lnd_id: channel_edge_update.chan_id}
+    channel_fields
   end
 
   defp maybe_add(statement, nil, _field) do
@@ -29,5 +28,17 @@ defmodule LightningGraph.Neo4j.Lnd.Mutations.Channel do
 
   defp maybe_add(statement, value, field) do
     ["#{field}: \"#{value}\"" | statement]
+  end
+
+  defp convert(channel_edge_update) do
+    routing_policy = channel_edge_update.routing_policy
+
+    %{
+      lnd_id: channel_edge_update.chan_id,
+      capacity: channel_edge_update.capacity,
+      base_fee: routing_policy.fee_base_msat,
+      fee_rate: routing_policy.fee_rate_milli_msat,
+      disabled: routing_policy.disabled
+    }
   end
 end
