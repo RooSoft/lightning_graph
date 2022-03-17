85da6511d11a8122f91effe851a37c7550e87164
RooSoft <marc@roosoft.com>
2022-03-17 09:13:03 -0400

    update the updated field on node update

--- lib/neo4j/lnd/mutations/node.ex
+++ lib/neo4j/lnd/mutations/node.ex
@@ -4,7 +4,11 @@ defmodule LightningGraph.Neo4j.Lnd.Mutations.Node do
   def update(conn, node_update) do
     query = """
     MATCH (n:node {pub_key:"#{node_update.identity_key}"})
-    SET n += { alias: "#{node_update.alias}", color: "#{node_update.color}" }
+    SET n += {
+      alias: "#{node_update.alias}",
+      color: "#{node_update.color}",
+      updated: "#{DateTime.utc_now() |> DateTime.to_string()}"
+    }
     RETURN n;
     """
 
